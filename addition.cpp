e4560b859c0b5abcceed5f8ff680c872deca055d
AjayRajput <140954519+ajayRajput3104@users.noreply.github.com>
2025-09-05 21:29:07 +0530

    added addition module

---
+++ addition.cpp
@@ -0,0 +1,18 @@
+/*
+ * File: addition.cpp
+ * Author: Ajay Rajput (Addition Module)
+ * Description:
+ *   This file contains the definition of the add function declared in addition.h.
+ *   The function performs the addition of two integers and returns the result.
+ *
+ * Example:
+ *   int sum = add(10, 5); // sum = 15
+ */
+
+#include "addition.h"
+
+// Function definition for addition
+int add(int a, int b) {
+    // Perform integer addition and return the result
+    return a + b;
+}
