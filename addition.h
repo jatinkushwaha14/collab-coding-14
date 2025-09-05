8cab9b16710d9de93202f01417c56d6b53fdebf4
AjayRajput <140954519+ajayRajput3104@users.noreply.github.com>
2025-09-05 21:23:42 +0530

    Add addition module

---
+++ addition.h
@@ -0,0 +1,21 @@
+#ifndef ADDITION_H
+#define ADDITION_H
+
+/*
+ * File: addition.h
+ * Author: Ajay Rajput (Addition Module)
+ * Description:
+ *   This header file declares the function prototype for the addition operation.
+ *   It is part of the collaborative Git assignment where each contributor
+ *   implements a different arithmetic operation in separate modules.
+ *
+ * Usage:
+ *   #include "addition.h"
+ *
+ *   int result = add(5, 3); // result will be 8
+ */
+
+// Function declaration for addition
+int add(int a, int b);
+
+#endif // ADDITION_H
